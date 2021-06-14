19551a87eb8df35877ee31c87a0f2c05eed3ae6b
Timothy Moss <tim@timothys-mbp.lan>
2021-06-14 18:27:09 +1000

    Addition of a hamburger menu in a new javascript file

--- hamburger.js
+++ hamburger.js
@@ -2,10 +2,14 @@ function menu() {
     const nav = document.getElementById('navbar')
     const bars = document.getElementById('bars')
     if (nav.style.display == 'none') {
-        bars.style.filter = 'brightness(0) invert(1)'
-        nav.style.display = 'flex'
+        bars.style.filter = 'brightness(0) invert(1)';
+        bars.style.transform = 'rotate(90deg)';
+        bars.style.transition = 'transform 200ms';
+        nav.style.display = 'flex';
     } else {
-        nav.style.display = 'none'
-        bars.style.filter = 'brightness(1) invert(0)'
+        nav.style.display = 'none';
+        bars.style.filter = 'brightness(1) invert(0)';
+        bars.style.transform = 'rotate(0deg)';
+        bars.style.transition = 'transform 200ms';
     }
 }
